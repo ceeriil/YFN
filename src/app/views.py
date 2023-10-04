d5f5392e2a7fbfb393022efe62a8d6d84c51b81c
Emmah-oo <emmaochonogor715@gmail.com>
2023-10-04 14:36:11 +0100

    finished trending and contact section

--- src/app/views.py
+++ src/app/views.py
@@ -32,5 +32,45 @@ class HomeView(TemplateView):
             },
         ]
 
+        trending_card_list = [
+            {
+                'image_url': '/static/app/img/writers2.png',
+                'alt_text': 'Card 1',
+                'card_title': 'Writers',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+            {
+                'image_url': '/static/app/img/creators2.png',
+                'alt_text': 'Card 2',
+                'card_title': 'Content Creators',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+            {
+                'image_url': '/static/app/img/gossip.png',
+                'alt_text': 'Card 3',
+                'card_title': 'Gossip',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+            {
+                'image_url': '/static/app/img/enterpreneur2.png',
+                'alt_text': 'Card 4',
+                'card_title': 'Enterpreneur',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+            {
+                'image_url': '/static/app/img/fashion.png',
+                'alt_text': 'Card 5',
+                'card_title': 'Fashion',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+            {
+                'image_url': '/static/app/img/artists2.png',
+                'alt_text': 'Card 6',
+                'card_title': 'Artists',
+                'desc': 'tands for “search engine dnjjsjjsjsj optimization.” In simple terms, it means the process of improving',
+            },
+        ]
+
         context['card_data_list'] = card_data_list
+        context['trending_card_list'] = trending_card_list
         return context
