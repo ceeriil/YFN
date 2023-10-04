85fc47bb03ea35bf62bf7f1dad3584796cd22768
Emmah-oo <emmaochonogor715@gmail.com>
2023-10-04 12:07:01 +0100

    finished about us and category section

--- src/app/views.py
+++ src/app/views.py
@@ -5,3 +5,32 @@ from app.vars import NAME
 
 class HomeView(TemplateView):
     template_name = f"{NAME}/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        card_data_list = [
+            {
+                'image_url': '/static/app/img/enterpreneur.png',
+                'alt_text': 'Card 1',
+                'card_title': 'Entrepreneurs',
+            },
+            {
+                'image_url': '/static/app/img/writers.png',
+                'alt_text': 'Card 2',
+                'card_title': 'Writers',
+            },
+            {
+                'image_url': '/static/app/img/creators.png',
+                'alt_text': 'Card 3',
+                'card_title': 'Content Creators',
+            },
+            {
+                'image_url': '/static/app/img/artists.png',
+                'alt_text': 'Card 4',
+                'card_title': 'Artists',
+            },
+        ]
+
+        context['card_data_list'] = card_data_list
+        return context
