6026bb0057cff5521351e2119662e5bb4216f427
Emmah-oo <emmaochonogor715@gmail.com>
2023-10-12 11:08:47 +0100

    added route for profile edit page and finished styling

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('sign_up/', views.sign_up, name='users-sign-up'),
         path('profile/', views.profile, name='users-profile'),
+        path('edit_profile/', views.edit_profile, name='edit-profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'),
          name='users-login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'),
